93a576caab525097c72e682ef9a82d0c6b2c88aa
livialeekt <98330263+livialeekt@users.noreply.github.com>
2023-07-12 08:45:37 -0600

    rename

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask
 
+from ChildProject.components import sorcery
+from ChildProject import food
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "<p>This is another parent project</p>"
+
+
+@app.route("/magic")
+def get_magic():
+    return sorcery.magic()
+
+
+@app.route("/magic")
+def get_special():
+    return sorcery.special()
+
+
+@app.route("/lunch")
+def get_lunch():
+    return food.lunch()
+
+
+@app.route("/dinner")
+def get_dinner():
+    return food.dinner()
 
 
 if __name__ == "__main__":
