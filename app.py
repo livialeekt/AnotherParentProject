2f90908736bba5e9c41b40db71d9e496d117e83c
livialeekt <98330263+livialeekt@users.noreply.github.com>
2023-07-12 09:59:23 -0600

    Added urls

--- app.py
+++ app.py
@@ -8,26 +8,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    # http://127.0.0.1:5001/
     return "<p>This is another parent project</p>"
 
 
 @app.route("/magic")
 def get_magic():
+    # http://127.0.0.1:5001/magic
     return sorcery.magic()
 
 
-@app.route("/magic")
+@app.route("/special")
 def get_special():
+    # http://127.0.0.1:5001/special
     return sorcery.special()
 
 
 @app.route("/lunch")
 def get_lunch():
+    # http://127.0.0.1:5001/lunch
     return food.lunch()
 
 
 @app.route("/dinner")
 def get_dinner():
+    # http://127.0.0.1:5001/dinner
     return food.dinner()
 
 
